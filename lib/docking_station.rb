60461e189e768d330782f000e671612575a8b873
Robert Jones <robert.g.jones@gmail.com>
2017-07-18 16:24:41 +0100

    Bike objects can be docked
    Objects can be stored in a #bikes attribute

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,19 @@
 class DockingStation 
 
+  attr_reader :bikes
+
+  def initialize
+    @bikes = [] 
+  end
+
   def release_bike
     raise 'No bikes available'
   end
+
+  def dock(bike)
+    self.bikes << bike 
+  end
+  
+  private
+  attr_writer :bikes
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,14 @@ require 'docking_station'
 
 describe DockingStation do
   subject(:dock) { described_class.new }
+  let(:bike) { double(:bike) }
+
+  describe "#dock" do
+   it "stores bike object" do
+      dock.dock(bike)
+      expect(dock.bikes).to include(bike)
+    end
+  end
 
   describe "#release" do
     it "raises exception to release if dock is empty" do
